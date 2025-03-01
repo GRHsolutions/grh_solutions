820068f4007881ba96a760df20ceb78e61100e22
miguel-ballesteros <124916979+miguel-ballesteros@users.noreply.github.com>
2025-02-28 20:54:46 -0500

    Ajusta el menu (sidebar y nadbar)

--- Grh-Solutions/grh-solutions.client/src/generics/SearchBar/search.tsx
+++ Grh-Solutions/grh-solutions.client/src/generics/SearchBar/search.tsx
@@ -1,35 +1,39 @@
-import Paper from '@mui/material/Paper';
-import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
+import Paper from "@mui/material/Paper"
+import InputBase from "@mui/material/InputBase"
+import Divider from "@mui/material/Divider"
+import IconButton from "@mui/material/IconButton"
+import SearchIcon from "@mui/icons-material/Search"
 
-interface SearchBarProps{
-    value:string,
-    onChange:(newValue: string) => void,
-    onSubmit:() => void
+interface SearchBarProps {
+  value: string;
+  onChange: (newValue: string) => void;
+  onSubmit: () => void;
 }
-export default function SearchBar({value,onChange,onSubmit}:SearchBarProps) {
+export default function SearchBar({
+  value,
+  onChange,
+  onSubmit,
+}: SearchBarProps) {
   return (
     <Paper
       component="form"
-      onSubmit={(e: any)=>{
+      onSubmit={(e: any) => {
         e.preventDefault();
         onSubmit();
-    }}
-      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+      }}
+      sx={{ p: "1px 2px", display: "flex", alignItems: "center", width: 350 }}
     >
-      <InputBase 
-      value={value}
-      onChange={(e: any)=> onChange(e.target.value as string)}
+      <InputBase
+        value={value}
+        onChange={(e: any) => onChange(e.target.value as string)}
         sx={{ ml: 1, flex: 1 }}
         placeholder="Search..."
-        inputProps={{ 'aria-label': 'search google maps' }}
+        inputProps={{ "aria-label": "search google maps" }}
       />
       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+      <IconButton type="button" sx={{ p: "5px" }} aria-label="search">
         <SearchIcon />
       </IconButton>
     </Paper>
-  );
+  )
 }
--- Grh-Solutions/grh-solutions.client/src/generics/sidebar/sideBar.tsx
+++ Grh-Solutions/grh-solutions.client/src/generics/sidebar/sideBar.tsx
@@ -1,57 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Styles from "./sideBar.module.scss";
-import { Backdrop, Button, IconButton } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
+import { Button } from "@mui/material";
 import { useRenderedItems } from "./renderedItems";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 export const SideBar: React.FC = () => {
-    const [collapse, setCollapse] = React.useState(false);
-    const { items } = useRenderedItems(); // Obtenemos los ítems dinámicos
+  const [collapse, setCollapse] = useState(false);
+  const [openSubmenus, setOpenSubmenus] = useState<{ [key: string]: boolean }>(
+    {}
+  );
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+  const [selectedSubItem, setSelectedSubItem] = useState<string | null>(null);
+  const { items } = useRenderedItems();
+  const toggleSubmenu = (to: string) => {
+    setOpenSubmenus((prev) => ({ ...prev, [to]: !prev[to] }));
+  };
 
-    return (
-        <>
-            <Button variant="text" onClick={() => setCollapse(true)} >
-                <MenuIcon fontSize="large" sx={{color: "blue"}}/>
-            </Button>
-
-            <Backdrop
-                open={collapse}
-                onClick={() => setCollapse(false)}
-                sx={{ zIndex: 150, backgroundColor: "rgba(0, 0, 0, 0.5)" }}
-            >
-                <aside
-                    className={`${Styles.sidebar} ${collapse ? Styles.open : Styles.closed}`}
-                    onClick={(e) => e.stopPropagation()}
-                >
-                    <IconButton className={Styles.closeButton} onClick={() => setCollapse(false)}>
-                        <CloseIcon />
-                    </IconButton>
-
-                    <div className={Styles.logo}>
-                        <h2>GRH Solutions</h2>
-                    </div>
-
-                    <nav className={Styles.nav}>
-                        <ul>
-                            {items.map((item) =>
-                                item.visible ? (
-                                    <li key={item.to}>
-                                        <Link to={item.to} className={item.active ? Styles.active : ""}>
-                                            {item.label}
-                                        </Link>
-                                    </li>
-                                ) : null
-                            )}
-                        </ul>
-                    </nav>
-
-                    <div className={Styles.footer}>
-                        <p>© 2023 Mi App</p>
-                    </div>
-                </aside>
-            </Backdrop>
-        </>
-    );
+  return (
+    <>
+      <Button variant="text" onClick={() => setCollapse(!collapse)}>
+        <MenuIcon fontSize="large" sx={{ color: "white" }} />
+      </Button>
+      <div
+        className={`${Styles.backdrop} ${
+          collapse ? Styles.open : Styles.closed
+        }`}
+      >
+        <aside
+          className={`${Styles.sidebar} ${
+            collapse ? Styles.open : Styles.closed
+          }`}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <nav className={Styles.nav}>
+            <ul>
+              {items.map((item) =>
+                item.visible ? (
+                  <li key={item.to}>
+                    {item.subItems && item.subItems.length > 0 ? (
+                      <div
+                        onClick={() => {
+                          toggleSubmenu(item.to);
+                          setSelectedItem(item.to);
+                        }}
+                        className={`${Styles.menuItem} ${
+                          selectedItem === item.to ? Styles.active : ""
+                        }`}
+                      >
+                        <div className={Styles.icons}>
+                          {item.icon}
+                          {item.label}
+                          <div className={`${Styles.arrow} ${selectedItem === item.to ? Styles.activateArrow : ""}`}>
+                            <KeyboardArrowRightIcon />
+                          </div>
+                        </div>
+                      </div>
+                    ) : (
+                      <Link
+                        to={item.to}
+                        className={`${Styles.link} ${
+                          selectedItem === item.to ? Styles.active : ""
+                        }`}
+                        onClick={() => setSelectedItem(item.to)}
+                      >
+                        <div className={Styles.icons}>
+                          {item.icon}
+                          {item.label}
+                        </div>
+                      </Link>
+                    )}
+                    {item.subItems && openSubmenus[item.to] && (
+                      <ul className={Styles.subMenu}>
+                        {item.subItems.map((subItem) => (
+                          <li key={subItem.to}>
+                            <Link
+                              to={subItem.to}
+                              onClick={() => setSelectedSubItem(subItem.to)}
+                              className={`${Styles.linkSubMenu} ${
+                                selectedSubItem === subItem.to
+                                  ? Styles.active
+                                  : Styles.linkSubMenu
+                              }`}
+                            >
+                              {subItem.label}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </li>
+                ) : null
+              )}
+            </ul>
+          </nav>
+        </aside>
+      </div>
+    </>
+  );
 };
