8db79e83e30289c9f95bc23de113be55ecabef9a
JuanS <126506142+ryazbo@users.noreply.github.com>
2025-03-17 08:36:20 -0500

    cambios en la raiz de todo el front end, uso de otro puerto de hosteo y  inicializacion del puerto para el backend 👍 🍎

--- Grh-Solutions/grh-solutions.client/vite.config.ts
+++ Grh-Solutions/grh-solutions.client/vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '^/api/': {
+        target: 'http://localhost:5300', // definir puerto donde el backend se aloja
+        secure: false,
+        ws: true,
+      }
+    },
+    port: 5200, // definir otro fucking puerto de ejecucion.
+  },
+  define: {
+    // Some libraries use the global object, even though it doesn't exist in the browser.
+    // Alternatively, we could add <script>window.global = window;</script> to index.html.
+    // https://github.com/vitejs/vite/discussions/5912
+    global: {},
+  },
+
 })
