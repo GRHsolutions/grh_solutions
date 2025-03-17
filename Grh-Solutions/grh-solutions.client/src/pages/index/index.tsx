d06ee8e07ed78a6613fced7b4f272da38d782e0a
miguel-ballesteros <miguelballesta05@gmail.com>
2025-03-16 20:47:36 -0500

    Ajusta la pagina principal y usar el cambio de color

--- Grh-Solutions/grh-solutions.client/src/pages/index/index.tsx
+++ Grh-Solutions/grh-solutions.client/src/pages/index/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
 import { Instagram, Facebook, LinkedIn, Twitter, YouTube, WhatsApp } from "@mui/icons-material";
+import { Box, useTheme } from "@mui/material";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
@@ -12,65 +13,92 @@ import disolinImage2 from '../../img/disolin2.png';
 import disolinImage3 from '../../img/disloin3.jpg';
 
 export default function Index() {
+  const theme = useTheme();
   return (
+
     <div className="background">
+      <div className="overlay"></div>
       <div className="content">
-        <h1 className="title">Disolin</h1>
+        <div className="main-container">
+          <div className="carousel-container">
+            <Swiper
+              modules={[Pagination, Navigation, Autoplay]}
+              spaceBetween={20}
+              slidesPerView={1}
+              loop={true}
+              autoplay={{ delay: 3000 }}
+              pagination={{ clickable: true }}
+              navigation
+            >
+              <SwiperSlide>
+                <img src={disolinImage} alt="Imagen 1" className="carousel-img" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={disolinImage2} alt="Imagen 2" className="carousel-img" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={disolinImage3} alt="Imagen 3" className="carousel-img" />
+              </SwiperSlide>
+            </Swiper>
+          </div>
 
-        {/* Carrusel de imágenes */}
-        <div className="carousel-container">
-          <Swiper
-            modules={[Pagination, Navigation, Autoplay]}
-            spaceBetween={20}
-            slidesPerView={1}
-            loop={true}
-            autoplay={{ delay: 3000 }}
-            pagination={{ clickable: true }}
-            navigation
-          >
-            <SwiperSlide>
-              <img src={disolinImage} alt="Imagen 1" className="carousel-img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={disolinImage2} alt="Imagen 2" className="carousel-img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={disolinImage3} alt="Imagen 3" className="carousel-img" />
-            </SwiperSlide>
-          </Swiper>
+          <div className="text-container">
+            <h1 className="title">Disolin</h1>
+            <p className="description">
+              Disolin es una empresa dedicada a la distribución y monitoreo de máquinas expendedoras.
+              Actualmente, se encuentra en crecimiento, pero enfrenta desafíos en la gestión de recursos humanos.
+            </p>
+          </div>
         </div>
-
-        <p className="description">
-          Disolin es una empresa dedicada a la distribución y monitoreo de máquinas expendedoras.
-          Actualmente, se encuentra en crecimiento, pero enfrenta desafíos en la gestión de recursos humanos.
-        </p>
       </div>
-
-      <footer className="footer">
-        <p className="contact-title"><strong>Contáctanos.</strong></p>
-
-        <div className="contact-section">
-          <div className="contact-info">
-            <p>
-              <strong>Correo:</strong>
-              <a href="mailto:disolin.asociados@gmail.com"> disolin.asociados@gmail.com</a>
-            </p>
-            <p>
-              <strong>Teléfono:</strong>
-              <a href="tel:86756434567"> 86756434567</a>
-            </p>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "start",
+          height: "100%",
+          width: "100%",
+          fontSize: "2rem",
+          fontFamily: theme.typography.fontFamily,
+          color: theme.palette.primary.contrastText,
+        }}
+      >
+        <footer className="footer">
+          <p className="contact-title"><strong>Contáctanos.</strong></p>
+          <div className="contact-section">
+            <div className="contact-info">
+              <p>
+                <strong>Correo:</strong>
+                <a href="mailto:disolin.asociados@gmail.com"> disolin.asociados@gmail.com</a>
+              </p>
+              <p>
+                <strong>Teléfono:</strong>
+                <a href="tel:86756434567"> 86756434567</a>
+              </p>
+            </div>
+            <div className="social-icons">
+              <a href="https://www.instagram.com" target="_blank">
+                <Instagram sx={{ color: theme.palette.text.primary }} />
+              </a>
+              <a href="https://www.facebook.com" target="_blank">
+                <Facebook sx={{ color: theme.palette.text.primary }} />
+              </a>
+              <a href="https://www.linkedin.com" target="_blank">
+                <LinkedIn sx={{ color: theme.palette.text.primary }} />
+              </a>
+              <a href="https://twitter.com" target="_blank">
+                <Twitter sx={{ color: theme.palette.text.primary }} />
+              </a>
+              <a href="https://www.youtube.com" target="_blank">
+                <YouTube sx={{ color: theme.palette.text.primary }} />
+              </a>
+              <a href="https://wa.me/86756434567" target="_blank">
+                <WhatsApp sx={{ color: theme.palette.text.primary }} />
+              </a>
+            </div>
           </div>
-
-          <div className="social-icons">
-            <a href="https://www.instagram.com" target="_blank"><Instagram /></a>
-            <a href="https://www.facebook.com" target="_blank"><Facebook /></a>
-            <a href="https://www.linkedin.com" target="_blank"><LinkedIn /></a>
-            <a href="https://twitter.com" target="_blank"><Twitter /></a>
-            <a href="https://www.youtube.com" target="_blank"><YouTube /></a>
-            <a href="https://wa.me/86756434567" target="_blank"><WhatsApp /></a>
-          </div>
-        </div>
-      </footer>
+        </footer>
+      </Box>
     </div>
   );
 }
--- Grh-Solutions/grh-solutions.client/src/pages/solicitudes/SolicitudesPages.tsx
+++ Grh-Solutions/grh-solutions.client/src/pages/solicitudes/SolicitudesPages.tsx
@@ -1,15 +1,28 @@
+import { Box } from "@mui/material"
 import CreatedSolicitudes from "./componentes/CreatedSolicitudes"
 import TableSolicitudes from "./componentes/tableSolicitudes"
 import "./stiles.scss"
-export default function SolicitudesPage (){
+export default function SolicitudesPage() {
   return (
+    <Box
+    sx={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "start",
+      height: "100%",
+      width: "100%",
+      fontSize: "2rem",
+    }}
+  >
     <div className="TableContent">
-        <h1 className="Title">Bienvenido al portal de Solicitudes y peticiones.</h1>
-        <div className="divButton">        <CreatedSolicitudes/></div>
+      <h1 className="Title">Bienvenido al portal de Solicitudes y peticiones.</h1>
+      <div className="divButton">
+        <CreatedSolicitudes />
+      </div>
 
-        
-        <TableSolicitudes/>
+
+      <TableSolicitudes />
     </div>
-
+    </Box>
   )
 }
