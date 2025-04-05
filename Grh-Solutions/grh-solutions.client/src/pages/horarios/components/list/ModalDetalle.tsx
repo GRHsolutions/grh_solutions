82df6aa6c72fc49ecf9c915b2ced96f18fcb470b
Deiby S <deyy81f@gmail.com>
2025-04-04 19:04:55 -0500

    a

--- Grh-Solutions/grh-solutions.client/src/pages/horarios/components/list/ModalDetalle.tsx
+++ Grh-Solutions/grh-solutions.client/src/pages/horarios/components/list/ModalDetalle.tsx
@@ -4,18 +4,12 @@ import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import Groups3Icon from "@mui/icons-material/Groups3";
 import GrhBasicMenu from "../../../../generics/grh-generics/menu";
-import AcUnitIcon from '@mui/icons-material/AcUnit';
-import {
-    Button,
-  IconButton,
-  MenuItem,
-  Select,
-  TextField,
-  useTheme,
-} from "@mui/material";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import { IconButton, useTheme } from "@mui/material";
 import { Horarios } from "../../../../domain/models/horarios/Horarios-entities";
 import formatearFecha from "../../../../utils/formatearFecha";
 import { TabConfig, TabsCompo } from "../../../../generics/tabs/tabs";
+import GrhButton from "../../../../generics/grh-generics/button";
 
 const style = {
   position: "absolute",
@@ -38,140 +32,171 @@ interface BasicModalProps {
 export default function BasicModal({ current, handleClose }: BasicModalProps) {
   const theme = useTheme();
 
-  const tabs : TabConfig[] =[
+  const tabs: TabConfig[] = [
     {
-        value: "1",
-        label: "detalle",
-        content: (
-          <Box>
-            <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", mb: 2 }}>
-            <Typography>
-                Imformacion</Typography>
-                <GrhBasicMenu 
+      value: "1",
+      label: "detalle",
+      content: (
+        <Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              mb: 2,
+            }}
+          >
+            <Typography>Imformacion</Typography>
+            <GrhBasicMenu
               optionsPosition={{
-                top: '2px',
-                left: '10px'
+                top: "2px",
+                left: "10px",
               }}
               items={[
                 {
                   icon: <AcUnitIcon />,
                   label: "Opcion del menu 1",
-                  onClick: () => {console.log("pressed")},
+                  onClick: () => {
+                    console.log("pressed");
+                  },
                   visible: true,
-                  disabled: false
+                  disabled: false,
                 },
                 {
                   icon: <AcUnitIcon />,
                   label: "Opcion del menu 2",
-                  onClick: () => {console.log("pressed")},
+                  onClick: () => {
+                    console.log("pressed");
+                  },
                   visible: true,
-                  disabled: false
+                  disabled: false,
                 },
                 {
                   icon: <AcUnitIcon />,
                   label: "Opcion del menu 3",
-                  onClick: () => {console.log("pressed")},
+                  onClick: () => {
+                    console.log("pressed");
+                  },
                   visible: true,
-                  disabled: false
-                }
-              ]}          
+                  disabled: false,
+                },
+              ]}
             />
-            </Box>
-            <Box sx={{
-                ml:5
-            }}>
-              <Typography sx={{mb:0.5}}>Asignado del grupo {current?.tipoHorario.nombre}</Typography>
-              <Typography sx={{mb:1}}>desde la fecha {formatearFecha(current?.fechaInicio, true, true)}  </Typography>
-              <Typography sx={{mb:1}}>hasta la fecha {formatearFecha(current?.fechaFin, true, true)}</Typography>
+          </Box>
+          <Box
+            sx={{
+              ml: 5,
+            }}
+          >
+            <Typography sx={{ mb: 0.5 }}>
+              Asignado del grupo {current?.tipoHorario.nombre}
+            </Typography>
+            <Typography sx={{ mb: 1 }}>
+              desde la fecha {formatearFecha(current?.fechaInicio, true, true)}{" "}
+            </Typography>
+            <Typography sx={{ mb: 1 }}>
+              hasta la fecha {formatearFecha(current?.fechaFin, true, true)}
+            </Typography>
+          </Box>
+        </Box>
+      ),
+    },
+    {
+      value: "2",
+      label: "Grupo",
+      content: (
+        <Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              mb: 2,
+            }}
+          >
+            <Typography
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              Listado de usuarios vinculados al horario
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "flex-end",
+              }}
+            >
+              <GrhButton
+                startIcon={<AcUnitIcon />}
+                label="vincular usuario"
+                variant="principal"
+                sx={{
+                  width: '200px'
+                }}
+              />
             </Box>
           </Box>
-        ),
+          <Box sx={{ border: "2px solid black", padding: 2, borderRadius: 2 }}>
+            <Typography sx={{ border: "1px solid gray", padding: 1 }}>
+              juan rodriguez-21314324
+            </Typography>
+            <Typography sx={{ border: "1px solid gray", padding: 1 }}>
+              pedro gomez-134557
+            </Typography>
+            <Typography sx={{ border: "1px solid gray", padding: 1 }}>
+              pedro pinilla-131455
+            </Typography>
+            <Typography sx={{ border: "1px solid gray", padding: 1 }}>
+              danna camargo-32536467
+            </Typography>
+            <Typography sx={{ border: "1px solid gray", padding: 1 }}>
+              camilo diaz-21325356
+            </Typography>
+          </Box>
+        </Box>
+      ),
     },
-    {
-        value:"2",
-        label:"Grupo",
-        content:(
-            <Box>
-                <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", mb: 2 }}> 
-                <Typography sx={{
-                    display: "flex",justifyContent:"center", alignItems:"center"
-                }}>
-                    Listado de usuarios vinculados al horario
-                </Typography>
-                <GrhBasicMenu 
-              optionsPosition={{
-                top: '2px',
-                left: '10px'
-              }}
-              items={[
-                {
-                  icon: <AcUnitIcon />,
-                  label: "Opcion del menu 1",
-                  onClick: () => {console.log("pressed")},
-                  visible: true,
-                  disabled: false
-                },
-                {
-                  icon: <AcUnitIcon />,
-                  label: "Opcion del menu 2",
-                  onClick: () => {console.log("pressed")},
-                  visible: true,
-                  disabled: false
-                },
-                {
-                  icon: <AcUnitIcon />,
-                  label: "Opcion del menu 3",
-                  onClick: () => {console.log("pressed")},
-                  visible: true,
-                  disabled: false
-                }
-              ]}          
-            />
-                </Box>
-                <Box sx={{ border: "2px solid black", padding: 2, borderRadius: 2 }}>
-                    <Typography sx={{ border: "1px solid gray", padding: 1 }}>juan rodriguez-21314324</Typography>
-                    <Typography sx={{ border: "1px solid gray", padding: 1 }}>pedro gomez-134557</Typography>
-                    <Typography sx={{ border: "1px solid gray", padding: 1 }}>pedro pinilla-131455</Typography>
-                    <Typography sx={{ border: "1px solid gray", padding: 1 }}>danna camargo-32536467</Typography>
-                    <Typography sx={{ border: "1px solid gray", padding: 1 }}>camilo diaz-21325356</Typography>
-                </Box>
-            </Box>
-        ),
-    },
-  ]
+  ];
   return (
     <div>
       <Modal open={current != null} onClose={handleClose}>
         <Box sx={style}>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-            <Groups3Icon
-              sx={{ fontSize: 40, color: theme.palette.text.primary }}
-            />
-            <Box>
-              <Typography
-                variant="h6"
-                fontWeight="bold"
-                color={theme.palette.text.primary}
-              >
-                Horario del {formatearFecha(current?.fechaInicio, true, false)} hasta {formatearFecha(current?.fechaFin, true, false)}
-              </Typography>
-              <Typography variant="body2" color={theme.palette.text.primary}>
-                Detalles y imformacion relevante del horario del area de indexacion
-              </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+              <Groups3Icon
+                sx={{ fontSize: 40, color: theme.palette.text.primary }}
+              />
+              <Box>
+                <Typography
+                  variant="h6"
+                  fontWeight="bold"
+                  color={theme.palette.text.primary}
+                >
+                  Horario del{" "}
+                  {formatearFecha(current?.fechaInicio, true, false)} hasta{" "}
+                  {formatearFecha(current?.fechaFin, true, false)}
+                </Typography>
+                <Typography variant="body2" color={theme.palette.text.primary}>
+                  Detalles y imformacion relevante del horario del area de
+                  indexacion
+                </Typography>
+              </Box>
             </Box>
+            <IconButton onClick={handleClose}>
+              <CloseIcon />
+            </IconButton>
           </Box>
-          <IconButton onClick={handleClose}>
-            <CloseIcon />
-          </IconButton>
-        </Box>
-        <TabsCompo tabs={tabs} />
+          <TabsCompo tabs={tabs} />
         </Box>
       </Modal>
     </div>
