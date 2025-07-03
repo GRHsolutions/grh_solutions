ca9f45b1fa5b6417d0df0c312848d8a52f45a3a3
jose-angel-videz-diaz <joseangelvidezdiaz@gmail.com>
2025-07-03 16:55:54 -0500

    contract routes

--- Grh-Solutions/grh-solutions.server/src/routes/routes.ts
+++ Grh-Solutions/grh-solutions.server/src/routes/routes.ts
@@ -6,8 +6,7 @@ import rol from "./rol";
 import Vacancies from "./vacancies";
 import TypeContract from "./typeContract";
 import postulante from "./postulante";
-import contract from "./contract"
-// import { validateToken } from '../middleware/tokens.middlewares';
+import { validateToken } from '../middleware/tokens.middlewares';
 // import { verifyPermissionHandler } from '../middleware/verifyPermission.middleware';
 import permission from "./permission";
 import schedules from "./schedules";
@@ -18,11 +17,13 @@ import puesto from "./puesto";
 import empleado from "./empleados";
 import news from "./news";
 import profileRoutes from "./profile";
-const routes = Router();
+import commentary from './commentary';
+import contract from "./contract"
 
+const routes = Router();
 // Apply middlewares globally to all routes
-// routes.use(validateToken);
-// routes.use(verifyPermissionHandler);
+routes.use(validateToken);
+//routes.use(verifyPermissionHandler);
 
 //Define routes
 routes.use("/login", Login);
@@ -40,6 +41,7 @@ routes.use("/group", group);
 routes.use("/puesto", puesto);
 routes.use("/empleados", empleado);
 routes.use("/news", news);
+routes.use('/commentary', commentary)
 routes.use("/profiles", profileRoutes);
 routes.use("/contract", contract);
 
