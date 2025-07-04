26de5b701453508f36d0f87dd95ade0e6ed3a7d0
Deiby S <deyy81f@gmail.com>
2025-07-04 00:41:30 -0500

    report en swagger

--- Grh-Solutions/grh-solutions.server/src/swagger/global.swagger.ts
+++ Grh-Solutions/grh-solutions.server/src/swagger/global.swagger.ts
@@ -1,14 +1,17 @@
 import { ContractPaths, ContractSchema } from "./contrato.swagger";
+import { ReportPaths, ReportSchema } from "./report.swagger";
 import { swaggerComponents, swaggerPaths } from "./initializations.swagger";
 
 const globalPaths = {
   ...swaggerPaths,
   ...ContractPaths,
+  ...ReportPaths
 };
 
 const globalComponents = {
   ...swaggerComponents,
   ...ContractSchema,
+  ...ReportSchema
 };
 
 export { globalPaths, globalComponents };
