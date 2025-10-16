3f1f20d7f351e55978fadb5d9ce6f3434c9302e2
ryazbo <126506142+ryazbo@users.noreply.github.com>
2025-10-16 18:22:37 -0500

    Erase node js directory -- fix

--- Grh-Solutions/execute.sh
+++
@@ -1,5 +0,0 @@
-# Abre una nueva ventana de PowerShell para ejecutar el frontend
-powershell.exe -Command "Start-Process powershell -ArgumentList 'npm run dev' -WorkingDirectory './grh-solutions.client'"
-
-# Abre otra nueva ventana de PowerShell para ejecutar el backend
-powershell.exe -Command "Start-Process powershell -ArgumentList 'npm run dev' -WorkingDirectory './grh-solutions.server'"
